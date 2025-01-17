c3e7c15cd63525797d7e020c3ac314c04a7a7fd0
Assdjo <assogbadjodieubeni@gmail.com>
2025-01-17 11:23:50 +0100

    methode FindBytitle

--- src/main/java/bj/highfiveuniversity/book/Repository/BookRepository.java
+++ src/main/java/bj/highfiveuniversity/book/Repository/BookRepository.java
@@ -1,9 +1,11 @@
 package bj.highfiveuniversity.book.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import bj.highfiveuniversity.book.models.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    Optional<Book>findByTitle(String title);
 }
