8271dbd3e8ca023873dc841a71a9501b82192e49
Assdjo <assogbadjodieubeni@gmail.com>
2025-01-17 11:23:34 +0100

    methode findByName Author

--- src/main/java/bj/highfiveuniversity/book/Repository/AuthorRepository.java
+++ src/main/java/bj/highfiveuniversity/book/Repository/AuthorRepository.java
@@ -1,9 +1,13 @@
 package bj.highfiveuniversity.book.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import bj.highfiveuniversity.book.models.Author;
 
+
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-    
+    Optional<Author> findByNom(String nom);
 }
